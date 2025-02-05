9d7f20ad79d48fd750539966ec160e82ef867e1b
Abhishek Choudhari <abhishek007coc@gmail.com>
2025-02-05 22:54:37 +0530

    Feat: Friend Request Accept API development completed

--- friends/admin.py
+++ friends/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
-
+from .models import FriendRequestModel
 # Register your models here.
+
+class FriendRequestAdmin(admin.ModelAdmin):
+    list_display=('sender', 'receiver', 'status', 'timestamp')
+    search_fields=('sender', 'receiver', 'status')
+    
+admin.site.register(FriendRequestModel, FriendRequestAdmin)
