498745997d5ab952a4eecc2392703c9e34dbc23b
Abhishek Choudhari <abhishek007coc@gmail.com>
2025-10-21 12:20:18 +0530

    Fix: Fixed Websocket connection to accept Auth Token so that Authenticated users can communicate over WebSockets

--- project_main/asgi.py
+++ project_main/asgi.py
@@ -12,12 +12,13 @@ from django.core.asgi import get_asgi_application
 from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.auth import AuthMiddlewareStack
 import friends.routing
+from .middleware import TokenAuthMiddleware
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'project_main.settings')
 
 application = ProtocolTypeRouter({
     "http": get_asgi_application(),
-    "websocket": AuthMiddlewareStack(
+    "websocket": TokenAuthMiddleware(
         URLRouter(
             friends.routing.websocket_urlpatterns
         )
---
+++ project_main/middleware.py
@@ -0,0 +1,32 @@
+from urllib.parse import parse_qs
+from channels.db import database_sync_to_async
+from rest_framework.authtoken.models import Token
+from django.contrib.auth.models import AnonymousUser
+
+@database_sync_to_async
+def get_user_from_token(token_key):
+    try:
+        token = Token.objects.get(key=token_key)
+        return token.user
+    except Token.DoesNotExist:
+        return AnonymousUser()
+    
+class TokenAuthMiddleware:
+
+    def __init__(self, app):
+        self.app = app
+
+    async def __call__(self, scope, receive, send):
+
+        if scope.get("type") == "websocket":
+            print(f'Server Logs: Query String Auth {scope.get("query_string", b"")}')
+            qs = scope.get("query_string", b"").decode()
+            params = parse_qs(qs)
+            token_list = params.get("token") or params.get("auth_token")
+            if token_list:
+                token_key = token_list[0]
+                scope["user"] = await get_user_from_token(token_key)
+            else:
+                scope["user"] = AnonymousUser()
+
+        return await self.app(scope, receive, send)
